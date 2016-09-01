2837792ef02db1e81435f1c79d53a1f3e9d846db
Matthew Wiesner <wiesner@jhu.edu>
2016-09-01 12:13:06 -0400

    Adds updated path.sh

--- path.sh
+++ path.sh
@@ -1,3 +1,7 @@
-root=/Users/Matthew_Wiesner/Matthew/Research/JHUWorkshop2016/GKT
+# CLSP Cluster Root
+root=`pwd`
+
+# Local root
+#root=/Users/Matthew_Wiesner/Matthew/Research/JHUWorkshop2016/GKT
 export PYTHONPATH=$PYTHONPATH:${root}/local:${root}/local/transducer
 
